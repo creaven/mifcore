d3be13b8189d2658a2be70ce9626a096604db25d
creaven <creavenmoro@gmail.com>
2010-05-24 16:15:07 +0400

    add id/store/retrieve functions

--- Source/Mif/Mif.js
+++ Source/Mif/Mif.js
@@ -6,23 +6,65 @@ description: define Mif object
 license: MIT-Style License (http://mifjs.net/license.txt)
 copyright: Anton Samoylov (http://mifjs.net)
 authors: Anton Samoylov (http://mifjs.net)
-requires: 
-  - core:1.2.4:*
+requires: [Stylesheet/Stylesheet]
 provides: Mif
  
 ...
 */
-
-var Mif = {
+(function(){
 	
-	id: function(id){
-		return Mif.ids[id];
+	var Mif = this.Mif = {
+		
+		version: 'dev'
+	
+	};
+	
+	//id
+	var ids = {};
+	Mif.id = function(){
+		if(arguments.length == 2){
+			ids[arguments[1]] = arguments[0];
+			return this;
+		}else{
+			return ids[arguments[0]];
+		}
+	};
+	
+	//uid
+	var items = {};
+	var uid = 0;
+	Mif.store = function(item){
+		items[++uid] = item;
+		item.UID = uid;
+		return this;
+	};
+	Mif.retrieve = function(uid){
+		return items[uid];
+	};
+	
+	Mif.sheet = new Stylesheet();
+	
+})();
+
+String.implement({
+
+	toMifImg: function(src){//for normal browser MifImage - object {name: base64_encoded_image}, for ie MifImage - url to mhtml file
+		if(!Browser.Engine.trident){
+			return src ? 'data:;base64,' + MifImg[this] : 'url(data:;base64,' + MifImg[this] + ')';
+		}else{
+			return src ? 'mhtml:' + MifImg + '!' + this : 'url(mhtml:' + MifImg + '!' + this + ')';
+		}
 	},
 	
-	ids: {},
+	repeat: function(times){
+		return new Array(times + 1).join(this);
+	}
 	
-	uids: {},
-	
-	UID: 0
-	
-};
+});
+
+Event.Keys.extend({
+	'pgdown': 34,
+	'pgup': 33,
+	'home': 36,
+	'end': 35
+});
